d9e6533fa3c55ed118c55a4c9390568977acd57d
Keidaten <jeremyalder@hotmail.fr>
2020-04-07 10:56:07 +0200

    Added possibilty ton recup pokemon data

--- node_src/app.js
+++ node_src/app.js
@@ -1,6 +1,6 @@
-
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 let connect = require("./connection.js")
 let config = require("./config.js")
@@ -13,21 +13,43 @@ app.engine("html", mustacheExpress())
   app.set('views', __dirname + '/views');
   app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  // enregister le nom du pokemon dans une variable
+  // Envoyer une requête à l'api PokeAPI pour récupérer les infos du pokemon envoyé en param_tre
+  // Renvoyer les informations du pokemon en paramètre au client
+  // declare la variable qui contient le nom de pokemon passé en parametre de requete
+  let pokemonChosen = req.query.pokemonName;
+
+  // methode appel axios
+  axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemonChosen)
+    .then((response) =>{
+      // handle success
+      //console.log(response.data);
+      let pokemonData = response.data;
+      res.json(pokemonData);
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+})
+
+app.get('/calendar', (req, res) => {
+  let date = new Date();
+  console.log("params ", req.query);
+  res.send('la requêquête du graal se passe le ' + date + " donc soyez prêts");
 
 })
 
 app.get("/hello", (req, res) => {
-  res.render('index', {name: "César"})
+  res.render('index', {name: "Jey"})
 })
 
 
 app.get('/todo', async (req, res) => {
 
   let {db_client, db_connection} = await connect()
-  
+
   db_connection.collection('todo').find({}).toArray((err, result) => {
     if(err) return console.log(err)
 
@@ -35,7 +57,7 @@ app.get('/todo', async (req, res) => {
 
     db_client.close()
     res.send(result)
-   
+
   })
 })
 
@@ -43,4 +65,3 @@ app.get('/todo', async (req, res) => {
 app.listen(config.port, function () {
   console.log(`Example app listening on port ${config.port} !`)
 })
-

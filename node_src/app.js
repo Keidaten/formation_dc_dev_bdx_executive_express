d1efb84e10f86e65854c3f4c0e4dca96b33ff970
Keidaten <jeremyalder@hotmail.fr>
2020-04-08 09:13:37 +0200

    added comments

--- node_src/app.js
+++ node_src/app.js
@@ -15,29 +15,25 @@ app.engine("html", mustacheExpress())
 
 //POKEMON
 app.get('/', function (req, res) {
-  let pokemonChosen = req.query.pokemonName;
+  let pokemonChosen1 = req.query.chooseYourPokemon1;                            //récupère le paramètre de requête pour le 1er pokemon
+  let pokemonChosen2 = req.query.chooseYourPokemon2;                            //récupère le paramètre de requête pour le 2e pokemon
 
-  axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemonChosen)
+
+  axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemonChosen1)               //méthode d'appele d'axios pour le pokemon 1
     .then((response) =>{
       // handle success
-      let pokemonData = response.data;
-      // res.json(pokemonData);
-      return pokemonData;
+      let pokemonData = response.data;                                          // récupère toutes les données du pokemon
+      let pokemonName = pokemonData.name                                        // récupère ne nom
+      let pokemonHP = pokemonData.stats[5].base_stat                            // récupère les HP
+      let pokemonATK = pokemonData.stats[4].base_stat                           // récupère l'ATK
+
+      res.json(pokemonHP);
     })
     .catch((error) => {
       // handle error
       console.log(error);
     })
-    // recuperer HP
-    .then((pokemonData)=>{
-      let pokemonName = pokemonData.name
-      let pokemonHP = pokemonData.stats[5].base_stat
-      let pokemonATK = pokemonData.stats[4].base_stat
-
-      res.json(pokemonATK);
-    })
-
-
+    // .then(axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemonChosen2))   //appel d'axios pour le pokemon 2
 
 
 })
